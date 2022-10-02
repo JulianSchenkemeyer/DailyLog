cc0d331237c72648f5006ec61c98c2a6ca0a15ca
Julian Schenkemeyer <julianschenkemeyer@fastmail.com>
2022-10-02 16:20:42 +0200

    Init DailyLog Project

---
+++ DailyLog/DailyLogApp.swift
@@ -0,0 +1,20 @@
+//
+//  DailyLogApp.swift
+//  DailyLog
+//
+//  Created by Julian Schenkemeyer on 02.10.22.
+//
+
+import SwiftUI
+
+@main
+struct DailyLogApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
