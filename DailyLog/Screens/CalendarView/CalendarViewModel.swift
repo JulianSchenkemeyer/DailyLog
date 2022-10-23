7ec10ff44a7a7acd9627708c6f3a26b3ac7fb818
Julian Schenkemeyer <julianschenkemeyer@fastmail.com>
2022-10-23 17:43:54 +0200

    Combine core data entries with calendarItem

--- DailyLog/Screens/CalendarView/CalendarViewModel.swift
+++ DailyLog/Screens/CalendarView/CalendarViewModel.swift
@@ -5,33 +5,65 @@
 //  Created by Julian Schenkemeyer on 09.10.22.
 //
 
+import CoreData
 import Foundation
 
 final class CalendarViewModel: ObservableObject {
-    let startDateWithPrefix: Date
-    let endDate: Date
+	let startDateWithPrefix: Date
+	let endDate: Date
 
-    @Published var calendarItems: [CalendarItem] = []
+	@Published var calendarItems: [CalendarItem] = []
 
-    init() {
-        let today = Date()
+	init() {
+		let today = Date()
+		startDateWithPrefix = today.startOfCalendarWithPrefixDays
+		endDate = today.endOfMonth
 
-        startDateWithPrefix = today.startOfCalendarWithPrefixDays
-        endDate = today.endOfMonth
+		// Initialise data
+		let days = getCoreDataEntriesForDisplayedItems()
+		calendarItems = createCalendarItemsForDisplayedMonth(start: startDateWithPrefix, end: endDate, cdData: days)
+	}
 
-        calendarItems = createCalendarItemsForDisplayedMonth(start: startDateWithPrefix, end: endDate)
-    }
+	func createCalendarItemsForDisplayedMonth(start: Date, end: Date, cdData: [Day]) -> [CalendarItem] {
+		var calendarItems: [CalendarItem] = []
 
-    func createCalendarItemsForDisplayedMonth(start: Date, end: Date) -> [CalendarItem] {
-        var calendarItems: [CalendarItem] = []
+		var cdDataIterator = 0
+		var calendarIterator = start
+		while calendarIterator < end {
+			guard cdData.count > cdDataIterator else {
+				let newCalendarItem = CalendarItem(date: calendarIterator)
+				calendarItems.append(newCalendarItem)
+				calendarIterator = Calendar.current.date(byAdding: .day, value: 1, to: calendarIterator)!
+				continue
+			}
 
-        var iterator = start
-        while iterator < end {
-            let newCalendarItem = CalendarItem(date: iterator)
-            calendarItems.append(newCalendarItem)
-            iterator = Calendar.current.date(byAdding: .day, value: 1, to: iterator)!
-        }
+			var data: Day?
+			if let cdDataDate = cdData[cdDataIterator].date, cdDataDate.dayInt == calendarIterator.dayInt {
+				data = cdData[cdDataIterator]
+				cdDataIterator += 1
+			}
 
-        return calendarItems
-    }
+			let newCalendarItem = CalendarItem(date: calendarIterator, cdData: data)
+			calendarItems.append(newCalendarItem)
+			calendarIterator = Calendar.current.date(byAdding: .day, value: 1, to: calendarIterator)!
+		}
+
+		return calendarItems
+	}
+
+	var coreDataRequest: NSFetchRequest<Day> {
+		let request = Day.fetchRequest()
+		request.sortDescriptors = [NSSortDescriptor(keyPath: \Day.date, ascending: true)]
+		request.predicate = NSPredicate(format: "(date >= %@) AND (date <= %@)",
+		                                startDateWithPrefix as CVarArg,
+		                                endDate as CVarArg)
+		return request
+	}
+
+	func getCoreDataEntriesForDisplayedItems() -> [Day] {
+		let viewContext = PersistenceController.shared.container.viewContext
+		let days = (try? viewContext.fetch(coreDataRequest)) ?? []
+
+		return days
+	}
 }
